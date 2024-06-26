53edaeb7579916346385afc586f2507c20d1edcc
KR-EGOIST <jhso5041@naver.com>
2024-06-26 11:49:17 +0900

    ✨ update : 3-10 유저 데이터 저장 , 유저 쿼리 세팅 , 핸들러에 적용하기 , 클라이언트 테스트(오류 확인)

--- src/handlers/user/initial.handler.js
+++ src/handlers/user/initial.handler.js
@@ -2,10 +2,19 @@ import { addUser } from '../../session/user.session.js';
 import { HANDLER_IDS, RESPONSE_SUCCESS_CODE } from '../../constants/handlerIds.js';
 import { createResponse } from '../../utils/response/createResponse.js';
 import { handlerError } from '../../utils/error/errorHandler.js';
+import { createUser, findUserByDeviceID, updateUserLogin } from '../../db/user/user.db.js';
 
 const initialHandler = async ({ socket, userId, payload }) => {
   try {
     const { deviceId } = payload;
+
+    let user = await findUserByDeviceID(deviceId);
+
+    if (!user) {
+      user = await createUser(deviceId);
+    } else {
+      await updateUserLogin(user.id);
+    }
 
     addUser(socket, deviceId);
 
@@ -13,7 +22,7 @@ const initialHandler = async ({ socket, userId, payload }) => {
     const initialResponse = createResponse(
       HANDLER_IDS.INITIAL,
       RESPONSE_SUCCESS_CODE,
-      { userId: deviceId },
+      { userId: user.id },
       deviceId,
     );
 
